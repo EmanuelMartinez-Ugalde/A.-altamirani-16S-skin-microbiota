a46c405c900478ed4134cb5ae9bf07e02009c69e
Emanuel Martínez-Ugalde <83722048+EmanuelMartinez-Ugalde@users.noreply.github.com>
2022-09-06 16:18:48 -0500

    Update Core features using QIIME2.R

--- Scripts/Core features using QIIME2.R
+++ Scripts/Core features using QIIME2.R
@@ -1,44 +1,79 @@
 ##########################################
 #
 #       A.altamirani skin microbiome          
-#       Martínez-Ugalde et al. 2022
+#       MartÃ­nez-Ugalde et al. 2022
 #           emartug@gmail.com
 #
 ##########################################
 
 
 #Core features for metamorphic and non-metamorphic samples in QIIME2
-qiime feature-table filter-samples \
---i-table filtrar-table.qza \
---m-metadata-file metadata_altamirani.txt \
---p-where "SampleType IN ('Metamorphic')" \
---o-filtered-table met-table.qza
-
-qiime feature-table summarize \
---i-table adult-table.qza \
---o-visualization met-table.qzv 
-
-
-qiime feature-table core-features \
---i-table met-table.qza \
---p-min-fraction 0.8 \
---p-max-fraction 1 \
---p-steps 20 \
---o-visualization met-core-table.qzv
 
 qiime feature-table filter-samples \
---i-table filtrar-table.qza \
+--i-table Merged/Ambystoma_table.qza \
 --m-metadata-file metadata_altamirani.txt \
---p-where "SampleType IN ('Non-metamorphic')" \
---o-filtered-table nmet-table.qza
+--p-where "SampleType IN ('Adult')" \
+--o-filtered-table met90-table.qza
 
 qiime feature-table summarize \
---i-table nmet-table.qza \
---o-visualization nmet-table.qzv
+--i-table met90-table.qza \
+--o-visualization met90-table.qzv
 
 qiime feature-table core-features \
---i-table  juvenile-table.qza \
---p-min-fraction 0.8 \
+--i-table met95-table.qza \
+--p-min-fraction 0.90 \
 --p-max-fraction 1 \
---p-steps 20 \
---o-visualization nmet-core-table-rare.qzv
+--p-steps 10 \
+--o-visualization met90-core-table.qzv
+
+qiime feature-table filter-samples \
+--i-table Merged/Ambystoma_table.qza \
+--m-metadata-file metadata_altamirani.txt \
+--p-where "SampleType IN ('Juvenile')" \
+--o-filtered-table nmet90-table.qza
+
+qiime feature-table summarize \
+--i-table nmet90-table.qza \
+--o-visualization nmet90-table.qzv
+
+qiime feature-table core-features \
+--i-table  nmet90-table.qza \
+--p-min-fraction 0.90 \
+--p-max-fraction 1 \
+--p-steps 10 \
+--o-visualization nmet90-core-table-rare.qzv
+
+
+qiime feature-table filter-samples \
+--i-table Merged/Ambystoma_table.qza \
+--m-metadata-file metadata_altamirani.txt \
+--p-where "SampleType IN ('Sediment')" \
+--o-filtered-table sed90-table.qza
+
+qiime feature-table summarize \
+--i-table sed90-table.qza \
+--o-visualization sed90-table.qzv
+
+qiime feature-table core-features \
+--i-table  sed-table.qza \
+--p-min-fraction 0.90 \
+--p-max-fraction 1 \
+--p-steps 10 \
+--o-visualization sed90-core-table-rare.qzv
+
+qiime feature-table filter-samples \
+--i-table Merged/Ambystoma_table.qza \
+--m-metadata-file metadata_altamirani.txt \
+--p-where "SampleType IN ('Water')" \
+--o-filtered-table wat90-table.qza
+
+qiime feature-table summarize \
+--i-table wat90-table.qza \
+--o-visualization wat90-table.qzv
+
+qiime feature-table core-features \
+--i-table  wat90-table.qza \
+--p-min-fraction 0.90 \
+--p-max-fraction 1 \
+--p-steps 10 \
+--o-visualization wat90-core-table-rare.qzv
